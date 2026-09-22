e0b89c95fbb986b9be5854b47d297bf2f1f6f94a
robsonsvicero <robsonsvicero@gmail.com>
2026-09-22 10:32:08 -0300

    Atualização de fonte-family

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,11 @@ module.exports = {
     content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
   theme: {
   	extend: {
+  		fontFamily: {
+  			sans: ['Manrope', 'sans-serif'],
+  			serif: ['Manrope', 'serif'],
+  			mono: ['Manrope', 'monospace'],
+  		},
   		borderRadius: {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
